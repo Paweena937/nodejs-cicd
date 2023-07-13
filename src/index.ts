94aee989701508b48d65891793ca54e8e576d2e5
Paweena937 <134252989+Paweena937@users.noreply.github.com>
2023-07-13 11:41:58 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+//max min avergq
+
+//min
+function minNumber (numbers:number) { 
+    return Math.min(numbers);
+}
+console.log(minNumber)
+
+//max
+
+function maxNumber(numbers:number) {
+    return Math.max(numbers);    
+}
+console.log(maxNumber)
+
+//Average
+
+function AverageNumber(number:number) {
+    return sum AverageNumber)
+}
